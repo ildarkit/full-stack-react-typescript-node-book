45142fdc4b35ddfc37023e05e3272ad1731d6510
Ildar Shamiev <shamief@yandex.ru>
2024-08-24 10:32:06 +0300

    p345 configureStore provider

--- chap12/super-forum-client/src/index.tsx
+++ chap12/super-forum-client/src/index.tsx
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+// import ReactModal from 'react-modal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
+
+// ReactModal.setAppElement('#root');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
