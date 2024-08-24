24cff73fa66659b39a7e419563e2cc443b643b9d
Ildar Shamiev <shamief@yandex.ru>
2024-08-24 12:22:41 +0300

    p348 mocking data for left menu

--- chap12/super-forum-client/src/components/LeftMenu.tsx
+++ chap12/super-forum-client/src/components/LeftMenu.tsx
@@ -1,9 +1,27 @@
+import {useState, useEffect} from 'react';
 import {useWindowDimentions} from "../hooks/useWindowDimensions";
+import {getCategories} from '../services/DataService';
+import Category from '../models/Category';
 
 const LeftMenu = () => {
   const {width} = useWindowDimentions();
+  const [categories, setCategories] = useState<JSX.Element>(
+    <div>Left Menu</div>
+  );
+
+  useEffect(() => {
+    getCategories()
+      .then((categories: Array<Category>) => {
+        const cats = categories.map(cat => { 
+          return <li key={cat.id}>{cat.name}</li>;
+        });
+        setCategories(<ul className="category">{cats}</ul>);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   if (width <= 768) return null;
-  return <div className="leftmenu">Left Menu</div>;
+  return <div className="leftmenu">{categories}</div>;
 };
 
 export default LeftMenu;
