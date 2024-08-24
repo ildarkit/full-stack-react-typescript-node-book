85ff17793d88b5662ca5da79ddacd78208a4f5db
Ildar Shamiev <shamief@yandex.ru>
2024-08-24 11:32:02 +0300

    p346 ErrorBoundary

--- chap12/super-forum-client/src/index.tsx
+++ chap12/super-forum-client/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+import ErrorBoundary from './components/ErrorBoundary';
 // import ReactModal from 'react-modal';
 
 const root = ReactDOM.createRoot(
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={configureStore()}>
-    <App />
+    <ErrorBoundary>{[<App key='App'/>]}</ErrorBoundary>
   </Provider>
 );
 
