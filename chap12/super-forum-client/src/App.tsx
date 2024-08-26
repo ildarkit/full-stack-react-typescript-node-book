fb07fb02ffa635296fc52006c51c8bff34ce5d27
Ildar Shamiev <shamief@yandex.ru>
2024-08-26 14:47:57 +0300

    p371 ThreadCard

--- chap12/super-forum-client/src/App.tsx
+++ chap12/super-forum-client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="categorythreads/:categoryId" element={<Home/>}/>
+        <Route path="/categorythreads/:categoryId" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
--- chap12/super-forum-client/src/components/areas/main/Main.tsx
+++ chap12/super-forum-client/src/components/areas/main/Main.tsx
@@ -1,10 +1,35 @@
+import {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import ThreadCard from './ThreadCard';
 import MainHeader from './MainHeader';
 import Category from '../../../models/Category';
+import {getThreadsByCategory} from '../../../services/DataService';
 
 const Main = () => {
-  return <main className="content">
-    <MainHeader category={new Category("1", "Main")}/>
-    </main>;
+  const {categoryId} = useParams();
+  const [category, setCategory] = useState<Category | undefined>();
+  const [threadCards, setThreadCards] = useState<Array<JSX.Element | null>>();
+  
+  useEffect(() => {
+    console.log("main categoryId", categoryId);
+
+    if (categoryId && Number(categoryId) > 0) {
+      getThreadsByCategory(categoryId).then((threads) => {
+        const cards = threads.map((thread) => {
+          return <ThreadCard key={`thread-${thread.id}`} thread={thread}/>;
+        });
+        if (!category) setCategory(threads[0].category);
+        setThreadCards(cards);
+      });
+    }
+  }, [categoryId]);
+
+  return (
+    <main className="content">
+      <MainHeader category={category}/>
+      <div>{threadCards}</div>
+    </main>
+  );
 };
 
 export default Main;
