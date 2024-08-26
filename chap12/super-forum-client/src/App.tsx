634fc9066a931b3f73eade02b49dbedcc6a9f0a0
Ildar Shamiev <shamief@yandex.ru>
2024-08-26 18:38:55 +0300

    p382 Thread title and category screen

--- chap12/super-forum-client/src/App.tsx
+++ chap12/super-forum-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/routes/Home';
+import Thread from './components/routes/thread/Thread';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/categorythreads/:categoryId" element={<Home/>}/>
+        <Route path="/thread/:id" element={<Thread/>}/>
       </Routes>
     </BrowserRouter>
   );
