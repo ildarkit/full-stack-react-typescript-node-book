3c22e2c9b3084c24114081267009801a0a943dc8
Ildar Shamiev <shamief@yandex.ru>
2024-08-26 10:21:11 +0300

    p368 MainHeader for category name

--- chap12/super-forum-client/src/components/areas/main/Main.tsx
+++ chap12/super-forum-client/src/components/areas/main/Main.tsx
@@ -1,5 +1,10 @@
+import MainHeader from './MainHeader';
+import Category from '../../../models/Category';
+
 const Main = () => {
-  return <main className="content">Main</main>;
+  return <main className="content">
+    <MainHeader category={new Category("1", "Main")}/>
+    </main>;
 };
 
 export default Main;
