83c4796e5d0195cce73253cb0eb42f18a9fa0901
Ildar Shamiev <shamief@yandex.ru>
2024-08-27 17:48:48 +0300

    p399 move redux dispatch to Login component

--- chap12/super-forum-client/src/components/areas/sidebar/SideBarMenus.tsx
+++ chap12/super-forum-client/src/components/areas/sidebar/SideBarMenus.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
@@ -19,17 +19,6 @@ const SideBarMenus = () => {
   const [showLogin, setShowLogin] = useState(false);
   const [showLogout, setShowLogout] = useState(false);
   const user = useSelector((state: AppState) => state.user);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch({
-      type: UserProfileSetType,
-      payload: {
-        id: 1,
-        userName: "testUser",
-      },
-    });
-  }, [dispatch]);
 
   const onClickToggleRegister = () => {
     setShowRegister(!showRegister);
