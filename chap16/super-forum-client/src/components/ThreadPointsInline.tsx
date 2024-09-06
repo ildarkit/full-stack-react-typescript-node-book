150b0039b2f9a74ff4011794afed1a0de0acb24f
Ildar Shamiev <shamief@yandex.ru>
2024-09-06 13:19:45 +0300

    p550 thread item vote buttons

--- chap16/super-forum-client/src/components/ThreadPointsInline.tsx
+++ chap16/super-forum-client/src/components/ThreadPointsInline.tsx
@@ -1,29 +1,103 @@
-import { FC } from "react";
+import React, { FC } from "react";
+import { gql, useMutation } from "@apollo/client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { ThreadPointsBarProps } from "./ThreadPointsBar";
+import {
+  faHeart,
+  faChevronUp,
+  faChevronDown
+} from "@fortawesome/free-solid-svg-icons";
+import "./ThreadPointsInline.css";
 
+const UpdateThreadItemPoint = gql`
+  mutation UpdateThreadItemPoint(
+    $userId: ID!
+    $threadItemId: ID!
+    $increment: Boolean!
+  ) {
+    updateThreadItemPoint(
+      userId: $userId
+      threadItemId: $threadItemId
+      increment: $increment
+    )
+  }
+`;
 
-const ThreadPointsInline: FC<ThreadPointsBarProps> = ({
+class ThreadPointsInlineProps {
+  points: number = 0;
+  userId?: string;
+  threadId?: string;
+  threadItemId?: string;
+  allowUpdatePoints?: boolean = false;
+  refreshThread?: () => void;
+}
+
+const ThreadPointsInline: FC<ThreadPointsInlineProps> = ({
   points,
-  responseCount
+  userId,
+  threadId,
+  threadItemId,
+  allowUpdatePoints,
+  refreshThread,
 }) => {
+  const [execUpdateThreadItemPoint] = useMutation(UpdateThreadItemPoint);
+  const onClickIncThreadItemPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    await execUpdateThreadItemPoint({
+      variables: {
+        userId,
+        threadItemId,
+        increment: true,
+      },
+    });
+    refreshThread && refreshThread();
+  };
+
+  const onClickDecThreadItemPoint = async (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    await execUpdateThreadItemPoint({
+      variables: {
+        userId,
+        threadItemId,
+        increment: false,
+      },
+    });
+    refreshThread && refreshThread();
+  };
+
   return (
-    <label
-      style={{
-        marginRight: ".75em",
-        marginTop: ".25em",
-      }}
-    >
-      {points || 0}
-      <FontAwesomeIcon
-        icon={faHeart}
-        className="points-icon"
-        style={{
-          marginLeft: ".2em",
-        }}
-      />
-    </label>
+    <span className="threadpointsinline-item">
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints ? "block" : "none"}` }}
+      >
+        <FontAwesomeIcon 
+          icon={faChevronUp}
+          className="point-icon"
+          onClick={onClickIncThreadItemPoint}
+        />
+      </div>
+      {points}
+      <div
+        className="threadpointsinline-item-btn"
+        style={{ display: `${allowUpdatePoints ? "block" : "none"}` }}
+      >
+        <FontAwesomeIcon 
+          icon={faChevronDown}
+          className="point-icon"
+          onClick={onClickDecThreadItemPoint}
+        />
+      </div>
+      <div className="threadpointsinline-item-btn">
+        <FontAwesomeIcon
+          icon={faHeart}
+          className="points-icon"
+        />
+      </div>
+    </span>
   );
 };
 
